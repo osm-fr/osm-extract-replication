1f4d8eae7c22bf095d241443e24c3275b6613822
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2021-11-08 20:58:07 +0000

    Add rotation of merge logs

--- osc_modif/rotate-replication-log.sh
+++ osc_modif/rotate-replication-log.sh
@@ -16,3 +16,16 @@ done
 
 mv replication.log replication.log.1
 touch replication.log
+
+if [ -e merge.log.1 ]; then
+  gzip merge.log.1
+fi
+
+for i in $(seq 30 -1 1); do
+  if [ -e merge.log.$i.gz ]; then
+    mv merge.log.$i.gz merge.log.$(($i+1)).gz;
+  fi
+done
+
+mv merge.log merge.log.1
+touch merge.log
