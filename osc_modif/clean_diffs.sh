ac85ed94960e5c05b22845f6e11de86185809bd0
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2026-01-21 20:23:22 +0100

    clean_diffs: Parallelize removal of country diffs

--- osc_modif/clean_diffs.sh
+++ osc_modif/clean_diffs.sh
@@ -18,12 +18,14 @@ for d in $(seq 1000 $oldest_dir); do
   compl_dir=$(printf "%03d/%03d" $(($d / 1000)) $(($d % 1000)))
   if [ -e planet/minute/$compl_dir ]; then
     echo $compl_dir
-    for c in $minute_dirs; do
-      if [ -e $c/$compl_dir ]; then
-        echo $c/$compl_dir
-        rm -rf $c/$compl_dir
-      fi
-    done
+#    for c in $minute_dirs; do
+#      if [ -e $c/$compl_dir ]; then
+#        echo $c/$compl_dir
+#        rm -rf $c/$compl_dir
+#      fi
+#    done
+
+    ls -d $minute_dirs | parallel -j 4 "[ -e '{}/$compl_dir' ] && rm -rf '{}/$compl_dir' || true"
     rm -rf planet/minute/$compl_dir
   fi
 done
